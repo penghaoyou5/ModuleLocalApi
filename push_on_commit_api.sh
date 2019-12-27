5b8587511c9d8f9ce2b77e80685efcb53fd9a61a
xiaopeng.guo <guoxiaopeng@kingsoft.com>
2019-12-27 14:09:56 +0800

    second 2 commit

--- push_on_commit_api.sh
+++ push_on_commit_api.sh
@@ -1,1 +1,18 @@
 #!/usr/bin/env bash
+commit_diff_content=`git diff origin/master...HEAD --name-status`
+for line in ${commit_diff_content}
+do
+#    echo ${line%/*}
+    if [[ ${line%/*} == *Api ]]
+    then
+        echo ${line%/*}
+#        echo "有以api结尾的文件改变"
+        git push -u origin master
+        echo 0
+        exit 1
+#        gradle task
+    fi
+done
+
+echo 1
+exit 0
